8b74af2bb49d25a00351a7cad8e9d0baa763df5e
Masahiro Tanaka <masahiro@asial.co.jp>
2014-05-11 17:10:39 +0900

    Fixed Hello World app

---
+++ 0-helloworld/www/components/loader.js
@@ -0,0 +1,26 @@
+/**
+ * This is an auto-generated file.
+ * Please do not edit by hand.
+ */
+(function() {
+  function loaderJs(path) {
+    var elm = document.createElement('script')
+    elm.setAttribute("src", path)
+    document.getElementsByTagName("head")[0].appendChild(elm)
+  }
+  function loaderCss(path) {
+    var elm = document.createElement('link')
+    elm.setAttribute("rel", "stylesheet")
+    elm.setAttribute("href", path)
+    document.getElementsByTagName("head")[0].appendChild(elm)
+  }
+
+  loaderJs("cordova.js");
+
+/*** <GENERATED> ***/
+
+/*** <Start:jquery> ***/
+loaderJs("components/jquery/dist/jquery.min.js");
+/*** <End:jquery> ***/
+
+})();
