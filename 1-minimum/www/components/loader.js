683520d032e61cc8259ddd6a3454fdc7d118f67d
Masahiro Tanaka <masahiro@asial.co.jp>
2014-05-27 23:53:42 +0900

    Misc fix

---
+++ 1-minimum/www/components/loader.js
@@ -0,0 +1,29 @@
+/**
+ * This is a auto-generated code by Monaca HTML5 component manager.
+ * Please do not edit by hand.
+ */
+
+/*** <GENERATED> ***/
+
+
+/*** <Start:monaca-cordova-loader> ***/
+/*** <Start:monaca-cordova-loader LoadJs:"components/monaca-cordova-loader/cordova-loader.js"> ***/
+(function(){
+  if ((navigator.userAgent.match(/Android/i)) || (navigator.userAgent.match(/iPhone|iPad|iPod/i))) {
+    if (typeof location.href === "string") {
+      var relativePath = location.href.split("/www")[1];
+      var paths = relativePath.split("/");
+      var cordovaJsUrl = ""; 
+      for (var i = 0; i < paths.length - 2; i++) {
+        cordovaJsUrl += "../";
+      }
+      document.write("<script src=\"" + cordovaJsUrl+ "cordova.js" + "\"></script>");
+    }
+  } else if ((navigator.userAgent.match(/MSIE\s10.0/)) && (navigator.userAgent.match(/Windows\sNT\s6.2/))) {
+    var elm = document.createElement('script');
+    elm.setAttribute("src", "cordova.js");
+    document.getElementsByTagName("head")[0].appendChild(elm);
+  };
+})();
+/*** <End:monaca-cordova-loader LoadJs:"components/monaca-cordova-loader/cordova-loader.js"> ***/
+/*** <End:monaca-cordova-loader> ***/
