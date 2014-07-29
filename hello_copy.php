e3e82dc3fbb8d61e42848ba8df728c3923faec95
URAMOTO Kazuhiro <kaz@asial.co.jp>
2014-07-29 14:36:20 +0900

    Add hello_copy.php to copy common files from Hello World to the other templates

---
+++ hello_copy.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Copy platforms and config.xml files from Hello World to the other templates.
+ *
+ * @author Asial
+ */
+$helloTemplate = '0-helloworld';
+
+chdir(__DIR__);
+
+foreach (glob('*-*', GLOB_ONLYDIR) as $dir) {
+  if ($dir != $helloTemplate && preg_match('/^\d+\-[a-z0-9\-]+$/', $dir)) {
+    $template = $dir;
+    echo "Copy to $template\n";
+
+    foreach (['platforms', 'config.android.xml', 'config.ios.xml'] as $path) {
+      system(sprintf('cp -r %s %s', escapeshellarg($helloTemplate . '/' . $path), escapeshellarg($template . '/')));
+    }
+  }
+}
+
+echo "Finished.\n";
