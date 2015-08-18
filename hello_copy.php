a4df9db02e19a3c447e16163608704e64971587e
URAMOTO Kazuhiro <kaz@asial.co.jp>
2015-08-18 20:36:43 +0900

    Update hello_copy.php to set name tag of config.xml

--- hello_copy.php
+++ hello_copy.php
@@ -11,11 +11,24 @@ chdir(__DIR__);
 foreach (glob('*-*', GLOB_ONLYDIR) as $dir) {
   if ($dir != $helloTemplate && preg_match('/^\d+\-[a-z0-9\-]+$/', $dir)) {
     $template = $dir;
-    echo "Copy to $template\n";
 
+    // Copy common files from Hello World
+    echo "Copy to $template\n";
     foreach (['platforms', 'config.xml'] as $path) {
       system(sprintf('cp -r %s %s', escapeshellarg($helloTemplate . '/' . $path), escapeshellarg($template . '/')));
     }
+
+    // Set name tag
+    $projectInfo = json_decode(file_get_contents($template . '/.monaca/project_info.json'), true);
+    if (!$projectInfo) {
+      throw new Exception('Failed to parse project_info.json in ' . $template);
+    }
+    
+    $configXmlFilename = $template . '/config.xml';
+    $configXml = file_get_contents($configXmlFilename);
+    $configXml = preg_replace('/<name>[\w ]+<\/name>/', '<name>' . $projectInfo['title-en'] . '</name>', $configXml);
+    $configXml = preg_replace('/<description>[\w ]*<\/description>/', '<description></description>', $configXml);
+    file_put_contents($configXmlFilename, $configXml);
   }
 }
 
