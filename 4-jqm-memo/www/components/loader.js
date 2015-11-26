de131963a46c3ae01112314a7cf67bf50cd2b912
Katsutoshi Makino <makki@asial.co.jp>
2015-11-26 15:11:33 +0900

    modified templates of loader.js

--- 4-jqm-memo/www/components/loader.js
+++ 4-jqm-memo/www/components/loader.js
@@ -21,7 +21,7 @@
       }
       document.write("<script src=\"" + cordovaJsUrl+ "cordova.js" + "\"></script>");
     }
-  } else if ((navigator.userAgent.match(/MSIE\s10.0/)) && (navigator.userAgent.match(/Windows\sNT\s6.2/))) {
+  } else if ( ((navigator.userAgent.match(/MSIE\s10.0/)) && (navigator.userAgent.match(/Windows\sNT\s6.2/)) ) || navigator.userAgent.match(/MSAppHost/) ) {
     var elm = document.createElement('script');
     elm.setAttribute("src", "cordova.js");
     document.getElementsByTagName("head")[0].appendChild(elm);
