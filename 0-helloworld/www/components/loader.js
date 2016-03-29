75ce988328c8ba7520e5ce2c28e4f37b9ba4224c
Takuya Watanabe <takuya@asial.co.jp>
2016-03-29 20:58:15 +0900

    Update cordova-loader.js to 1.0.3

--- 0-helloworld/www/components/loader.js
+++ 0-helloworld/www/components/loader.js
@@ -25,7 +25,7 @@
     return device;
   }
     
-  if (/^https:\/\/preview-.+monaca\.(local||mobi)/.test(location.href)) {
+  if (location && typeof location.href === "string" && /^https:\/\/preview-.+monaca\.(local||mobi)/.test(location.href)) {
     window.device = getDeviceObjectForPreview();
   }
 
--- 0-helloworld/www/components/monaca-cordova-loader/cordova-loader.js
+++ 0-helloworld/www/components/monaca-cordova-loader/cordova-loader.js
@@ -1,4 +1,24 @@
 (function(){
+  function getDeviceObjectForPreview() {
+    var raw_values = window.location.search.substring(1).split('&');
+    var values = {};
+    var device = { platform: "" };
+    
+    if (raw_values) {
+      for (var key in raw_values) {
+        var tmp = raw_values[key].split('=');
+        values[tmp[0]] = decodeURIComponent(tmp[1]);
+      }
+      device.platform = values.platform;
+    }
+    
+    return device;
+  }
+    
+  if (location && typeof location.href === "string" && /^https:\/\/preview-.+monaca\.(local||mobi)/.test(location.href)) {
+    window.device = getDeviceObjectForPreview();
+  }
+
   if ((navigator.userAgent.match(/Android/i)) || (navigator.userAgent.match(/iPhone|iPad|iPod/i))) {
     if (typeof location.href === "string") {
       var relativePath = location.href.split("/www")[1];
@@ -9,7 +29,7 @@
       }
       document.write("<script src=\"" + cordovaJsUrl+ "cordova.js" + "\"></script>");
     }
-  } else if ((navigator.userAgent.match(/MSIE\s10.0/)) && (navigator.userAgent.match(/Windows\sNT\s6.2/))) {
+  } else if ( (navigator.userAgent.match(/MSIE\s10.0/) && navigator.userAgent.match(/Windows\sNT\s6.2/)) || navigator.userAgent.match(/MSAppHost/)) {
     var elm = document.createElement('script');
     elm.setAttribute("src", "cordova.js");
     document.getElementsByTagName("head")[0].appendChild(elm);
